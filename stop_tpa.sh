75a97f63338cd2b77720e89944b9d2d459d764ff
Vikram N <t.naganna@gmail.com>
2017-06-05 23:39:23 +0530

    Update stop_tpa.sh

--- stop_tpa.sh
+++ stop_tpa.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-cd ~/grails-projects/tplogin
 kill -9 $(ps -aef | grep node | grep -v grep | awk '{ print $2}')
 kill -9 $(ps -aef | grep tplogin.Application | grep -v grep | awk '{ print $2}')
